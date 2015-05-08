ebe02a0f6d9bbf6ca6616a3008595945c8205818
Ataru Kodaka <ataru.kodaka@gmail.com>
2015-05-08 14:49:48 +0000

    search

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+require 'nokogiri'
+require 'open-uri'
+
 
 module YahuokuBusters
   class App < Padrino::Application
@@ -13,6 +16,7 @@ module YahuokuBusters
       erb :top, locals: {recommended_users: ["no_regret_y", "miyuria0905", "gurei2200"] }
     end
 
+    ## user
     post '/user' do
       user = h(params[:user])
       redirect "/user/#{user}"
@@ -24,10 +28,36 @@ module YahuokuBusters
       show_user_auctions(params[:user], params[:page].to_i)      
     end
 
-    def show_user_auctions(user, page=1)
-      require 'nokogiri'
-      require 'open-uri'
+    ## search
+    post '/search' do
+      url = "http://auctions.search.yahoo.co.jp/search?p=#{ERB::Util.url_encode(params[:keyword])}"
+      doc = Nokogiri::HTML.parse(open(url))
 
+      i = 1
+      
+      ar = doc.search("div.a1wrp").map do |element|
+        ## title
+        elem = element.search("h3/a")
+        title = elem.inner_html
+        href = elem.attribute('href').value
+        href =~ /page(\d+)\.auctions.yahoo.co.jp/
+        server_number = $1
+        href =~ %r{auctions\.yahoo\.co\.jp/jp/auction/([a-z0-9][\d]+)}
+        a_id = $1
+        url = href
+
+        ## user
+        elem = element.search("div.a4/p/a")[1]
+        href = elem.attribute('href').value
+        href =~ %r{auctions\.yahoo\.co\.jp/user/([a-zA-Z0-9\-_]+)}
+        user = $1
+        {number: i, url: url, a_id: a_id, server_number: server_number, title: title, user: user}.tap {i = i + 1}
+      end
+      erb :search, locals: {keyword: params[:keyword], auction_list: ar}
+    end
+
+    ## functions
+    def show_user_auctions(user, page=1)
       per_page = 20  # per page
       b = (page-1) * per_page + 1  # start
       i = b
@@ -36,7 +66,7 @@ module YahuokuBusters
 
       ar = doc.search("div#list01//.a1//h3//a").map do |elem|
         url = elem.attribute('href').value
-        url =~ %r{/([a-z][0-9]+)}
+        url =~ %r{/([a-z0-9][0-9]+)}
         a_id = $1
         URI.split(url)[2] =~ /page(\d+)/
         server_number = $1
